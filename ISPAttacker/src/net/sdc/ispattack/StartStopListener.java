06789f8a969aec63aaee8b1b6b9cef35919dd82d
Kyle Malinowski <kmalino66@gmail.com>
2017-04-01 14:26:14 -0500

    finished some GUI touches

---
+++ ISPAttacker/src/net/sdc/ispattack/StartStopListener.java
@@ -0,0 +1,46 @@
+package net.sdc.ispattack;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class StartStopListener implements MouseListener{
+
+	private GuiMain thread;
+	
+	public StartStopListener(GuiMain thread)
+	{
+		this.thread = thread;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+		thread.switchRunning();
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
