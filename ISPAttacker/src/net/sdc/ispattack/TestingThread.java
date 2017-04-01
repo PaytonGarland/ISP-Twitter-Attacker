013c1ce4df1866335b979220465a14b73b7206ef
Kyle Malinowski <kmalino66@gmail.com>
2017-04-01 14:42:58 -0500

    Worked on frame

---
+++ ISPAttacker/src/net/sdc/ispattack/TestingThread.java
@@ -0,0 +1,27 @@
+package net.sdc.ispattack;
+
+import java.util.TimerTask;
+
+public class TestingThread extends TimerTask{
+
+	private GuiMain master;
+	
+	public TestingThread(GuiMain master)
+	{
+		this.master = master;
+		
+	}
+
+	@Override
+	public void run() {
+		
+		if(master.isRunning())
+		{
+			//TODO test the network speed
+			
+		}
+		
+	}
+	
+	
+}
